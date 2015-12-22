a35d242344844dbfed09980a31e7f2c62725f3bc
Mauricio Krzesinsk Junior <mauriciokj@gmail.com>
2015-12-22 03:57:26 -0200

    passando a imagem como parametro

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -42,6 +42,8 @@ class CardsController < ApplicationController
   # PATCH/PUT /cards/1.json
   def update
     respond_to do |format|
+      puts ">>>>>>>>>>"
+      puts params.inspect
       if @card.update(card_params)
         format.html { redirect_to @card, notice: 'Card was successfully updated.' }
         format.json { render :show, status: :ok, location: @card }
