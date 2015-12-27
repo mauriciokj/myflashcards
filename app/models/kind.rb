68b824d7b9a132c122a618616acc78bfaff25073
Mauricio Krzesinsk Junior <mauriciokj@gmail.com>
2015-12-27 02:49:27 -0200

    change background collors

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,5 +1,5 @@
 class Kind < ActiveRecord::Base
-  COLORS = ['red', 'blue']
+  COLORS = ['red', 'blue', 'yellow', 'grey', 'green']
   has_many :cards
   belongs_to :user
   validates :name, :color, presence: true
