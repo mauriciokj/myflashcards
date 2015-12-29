1c067ea5be13907576092095531d8345a561463c
Mauricio Krzesinsk Junior <mauriciokj@gmail.com>
2015-12-29 00:47:51 -0200

    kinds status

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -3,4 +3,16 @@ class Kind < ActiveRecord::Base
   has_many :cards
   belongs_to :user
   validates :name, :color, presence: true
+
+  def number_of_times_played
+    self.cards.sum(:number_of_times_played)
+  end
+
+  def number_of_correct_answers
+    self.cards.sum(:number_of_correct_answers)
+  end
+
+  def number_of_errors
+    self.cards.sum(:number_of_errors)
+  end
 end
